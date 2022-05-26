f1fa66fedf06b400a4afa12c292e69f0db1574e9
Eko <yteko12@gmail.com>
2022-05-26 08:24:17 +0700

    View data dan recylerview

---
+++ app/src/main/java/com/eko/viewdata/Detail_Buah_Activity.kt
@@ -0,0 +1,18 @@
+package com.eko.viewdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail_buah.*
+
+class Detail_Buah_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_buah)
+
+        var tangkap = intent.getStringExtra("txt")
+        var tangkap2 = intent.getIntExtra("img",0)
+        //menangkap data intent dan di set ke layout
+        detailtxt.text = tangkap
+        imgdetail.setImageResource(tangkap2)
+    }
+}
